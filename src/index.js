ad24f1caa7e631654068e37536993f34e5da43c6
zhus-k <76853151+zhus-k@users.noreply.github.com>
2021-09-14 14:24:23 -0400

    enable logging in dev env, update gitignore

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'; 
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
-import ThunkMiddleware from 'redux-thunk';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
 import { searchRobots, requestRobots } from './reducers';
 
-const logger = createLogger();
+let middleware = [];
+
+if (process.env.NODE_ENV !== 'production') {
+  const logger = createLogger();
+  middleware = [...middleware, logger];
+}
 
 const rootReducer = combineReducers({ searchRobots, requestRobots });
-const store = createStore(rootReducer, applyMiddleware(ThunkMiddleware ,logger));
+const store = createStore(rootReducer, applyMiddleware(thunk, ...middleware));
 
 ReactDOM.render(
   <Provider store={store}>
