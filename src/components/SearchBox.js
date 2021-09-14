5b008f5f2e872c93b179db32326222da58a3c96b
zhus-k <76853151+zhus-k@users.noreply.github.com>
2021-09-13 22:44:55 -0400

    Size adjustments

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({ searchfield, searchChange }) => {
     return (
         <div className='pa2'>
             <input
-                className='pa3 ba b--green bg-lightest-blue code'
+                className='pa3 ba b--green bg-lightest-blue code searchbox'
                 type='search'
                 placeholder='search robots'
                 onChange={ searchChange }
