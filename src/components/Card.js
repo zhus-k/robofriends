c74e11db48bd2edbf9dc496d1c3dfe554eea1b10
zhus-k <76853151+zhus-k@users.noreply.github.com>
2021-09-14 21:55:42 -0400

    updated card.js
    improved layout, downgraded component, animated infobox on hover/tap

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,52 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Card extends Component {
-    constructor(){
-        super();
-        this.state = {
-            isHovered: false,
-            timeoutID: 0,
-        }
-    }
-
-    // Sets a delay before setting isHovered
-    onEnter = (event) => {
-        this.setState({timeoutID: setTimeout(() => {
-            this.setState({isHovered: true});
-        }, 1000)})
-        
-    }
-
-    // Resets a card's isHovered state
-    resetCard = (delay) => {
-        clearTimeout(this.state.timeoutID);
-        this.setState({timeoutID: 0, isHovered: false});
-    }
-
-    render(){
-        const { name, email, id, phone, website } = this.props;
-
-        return (
-            this.state.isHovered
-            ?
-            <div className='bg-light-yellow dib br3 pa3 ma2 grow bw2 shadow-5 tc grid-item' onMouseLeave={ this.resetCard } >
-                <div className='br2 shadow-1 infobox'>
-                    <h2 className=''>{ name }</h2>
-                    <p className=''>{ email }</p>
-                    <p className=''>{ phone }</p>
-                    <p className=''>{ website }</p>
-                </div>
+const Card = ({ name, email, id, phone, website }) => {
+    return (
+        <div className='bg-light-yellow br3 pa3 ma2 grow bw2 shadow-5 tc card'>
+            <img src={`https://robohash.org/${id}?200x200`} alt="" />
+            <div className='br2 shadow-1 infobox overlay'>
+                <h2 className='text nameplate'>{ name }</h2>
+                <p className='text'>{ email }</p>
+                <p className='text'>{ phone }</p>
+                <p className='text'>{ website }</p>
             </div>
-            :
-            <div className='bg-light-yellow dib br3 pa3 ma2 grow bw2 shadow-5 tc grid-item' onMouseEnter={ this.onEnter } onMouseLeave={ this.resetCard }>
-                <img src={`https://robohash.org/${id}?200x200`} alt="" />
-                <div className='br2 shadow-1 infobox'>
-                    <h2 className=''>{ name }</h2>
-                    <p className=''>{ email }</p>
-                </div>
-            </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Card;
