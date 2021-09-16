a6dc777047a09faf66668937fba5ef6611936c09
zhus-k <76853151+zhus-k@users.noreply.github.com>
2021-09-16 13:51:18 -0400

    more rectangular cards, some css edits

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Card = ({ name, email, id, phone, website }) => {
     return (
-        <div className='bg-light-yellow br3 pa3 ma2 grow bw2 shadow-5 tc card'>
+        <div className='bg-light-yellow br1 pa3 ma2 grow bw2 shadow-5 tc card'>
             <img src={`https://robohash.org/${id}?200x200`} alt="" />
-            <div className='br2 shadow-1 infobox overlay'>
+            <div className='br1 shadow-1 infobox overlay'>
                 <h2 className='text nameplate'>{ name }</h2>
                 <p className='text'>{ email }</p>
                 <p className='text'>{ phone }</p>
